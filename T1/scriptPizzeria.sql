f0d67d880e5e8c3d681bf1b2c21c4215ec38d55c
clareta16 <clara.pages16@gmail.com>
2024-07-03 08:46:14 +0200

    Update scriptPizzeria.sql

--- T1/scriptPizzeria.sql
+++ T1/scriptPizzeria.sql
@@ -8,23 +8,21 @@ CREATE TABLE Pizzeria.client (
 	codiPostal TINYINT(5) NOT NULL,
 	tlfn INT(12) NULL,
 	localitat_id INT NULL,
-	provincia_id INT NULL,
 	CONSTRAINT client_pk PRIMARY KEY (id_client),
-	CONSTRAINT client_localitat_FK FOREIGN KEY (localitat_id) REFERENCES Pizzeria.localitat(id_localitat),
-	CONSTRAINT client_provincia_FK FOREIGN KEY (provincia_id) REFERENCES Pizzeria.provincia(id_provincia)
+	CONSTRAINT client_localitat_FK FOREIGN KEY (localitat_id) REFERENCES Pizzeria.localitat(id_localitat)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
 DROP TABLE IF EXISTS Pizzeria.client;
-CREATE INDEX client_id_client_IDX USING BTREE ON Pizzeria.client (id_client,cognoms);
+CREATE INDEX client_id_client_IDX USING BTREE ON Pizzeria.client (id_client, cognoms);
 
 -- Pizzeria.localitat definition
 
 CREATE TABLE Pizzeria.localitat (
 	id_localitat INT auto_increment NOT NULL,
 	nomLocalitat varchar(15) NOT NULL,
-	provincia_id varchar(100) NULL,
+	provincia_id INT NULL,
 	CONSTRAINT localitat_unique UNIQUE KEY (id_localitat),
 	CONSTRAINT localitat_provincia_FK FOREIGN KEY (provincia_id) REFERENCES Pizzeria.provincia(id_provincia)
 )
@@ -57,7 +55,7 @@ CREATE TABLE Pizzeria.comanda (
 	categoria varchar(100) NULL,
 	producte_id varchar(100) NULL,
 	reparto_id varchar(100) NULL,
-	client_id varchar(100) NULL,
+	client_id INT NULL,
 	CONSTRAINT comanda_pk PRIMARY KEY (id_comanda),
 	CONSTRAINT comanda_ComandaDomicili_FK FOREIGN KEY (reparto_id) REFERENCES Pizzeria.ComandaDomicili(id_reparto),
 	CONSTRAINT comanda_client_FK FOREIGN KEY (client_id) REFERENCES Pizzeria.client(id_client)
@@ -73,7 +71,7 @@ CREATE INDEX comanda_categoria_IDX USING BTREE ON Pizzeria.comanda (categoria);
 CREATE TABLE Pizzeria.productesDemanats (
 	id_producteDemanat INT auto_increment NOT NULL,
 	nomProducte varchar(20) NOT NULL,
-	id_comanda INT auto_increment NOT NULL,
+	id_comanda INT NOT NULL,
 	quantitat TINYINT(10) NOT NULL,
 	id_producte varchar(100) NULL,
 	CONSTRAINT productesDemanats_pk PRIMARY KEY (id_producteDemanat),
@@ -88,13 +86,13 @@ CREATE INDEX productesDemanats_id_producte_IDX USING BTREE ON Pizzeria.productes
 
 -- Pizzeria.tipusProducte definition
 
-CREATE TABLE `tipusProducte` (
-  `id_producte` int(11) NOT NULL AUTO_INCREMENT,
-  `tipusProducte` varchar(100) NOT NULL,
-  `descripcio` text DEFAULT NULL,
-  `imatge.jpg` varchar(200) NOT NULL,
-  `preu` decimal(4,2) NOT NULL,
-  PRIMARY KEY (`id_producte`)
+CREATE TABLE tipusProducte (
+	id_producte INT auto_increment NOT NULL,
+	tipusProducte varchar(100) NOT NULL,
+	descripcio text DEFAULT NULL,
+	imatge.jpg varchar(200) NOT NULL,
+	preu DECIMAL(4,2) NOT NULL,
+	PRIMARY KEY (id_producte)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Pizzeria.ComandaDomicili definition
@@ -102,7 +100,7 @@ CREATE TABLE `tipusProducte` (
 CREATE TABLE Pizzeria.ComandaDomicili (
 	id_reparto INT auto_increment NOT NULL,
 	id_repartidor INT auto_increment NOT NULL,
-	`data` DATE NOT NULL,
+	data DATE NOT NULL,
 	treballador_id varchar(100) NULL,
 	CONSTRAINT ComandaDomicili_PK PRIMARY KEY (id_reparto),
 	CONSTRAINT ComandaDomicili_treballador_FK FOREIGN KEY (treballador_id) REFERENCES Pizzeria.treballador(id_treballador)
@@ -132,11 +130,9 @@ CREATE TABLE Pizzeria.botiga (
 	id_botiga INT auto_increment NOT NULL,
 	adreca varchar(100) NOT NULL,
 	codiPostal TINYINT(5) NOT NULL,
-	localitat_id varchar(100) NULL,
-	provincia_id varchar(100) NULL,
+	localitat_id INT NULL,
 	CONSTRAINT botiga_pk PRIMARY KEY (id_botiga),
-	CONSTRAINT botiga_localitat_FK FOREIGN KEY (localitat_id) REFERENCES Pizzeria.localitat(id_localitat),
-	CONSTRAINT botiga_provincia_FK FOREIGN KEY (provincia_id) REFERENCES Pizzeria.provincia(id_provincia)
+	CONSTRAINT botiga_localitat_FK FOREIGN KEY (localitat_id) REFERENCES Pizzeria.localitat(id_localitat)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -159,8 +155,6 @@ COLLATE=utf8mb4_general_ci;
 DROP TABLE IF EXISTS Pizzeria.treballador;
 CREATE UNIQUE INDEX treballador_id_treballador_IDX USING BTREE ON Pizzeria.treballador (id_treballador);
 CREATE UNIQUE INDEX treballador_nif_IDX USING BTREE ON Pizzeria.treballador (nif);
-
-
 
 
 
